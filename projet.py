b7eae6552a7498487777da7e41351b72fcc56b22
selma.zine-eddine@edu.esiee.fr <selma.zine-eddine@edu.esiee.fr>
2023-11-29 10:18:36 +0100

    Structure

--- projet.py
+++ projet.py
@@ -1,1 +1,7 @@
-#erfd
+# 1 -Import et definitions des variables globales
+
+# 2 -Definition des fonctions secondaires
+
+# 3 -Definition du main() qui appellent les fonctions secondaires
+
+# 4 -Appel protégé du main()
