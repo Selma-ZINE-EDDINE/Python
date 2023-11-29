ee1543c37d313c0e44f10c494f27d704870cf409
selma.zine-eddine@edu.esiee.fr <selma.zine-eddine@edu.esiee.fr>
2023-11-29 11:28:08 +0100

    Ouverture du fichier
    Co-authored-by: Soso <SophiePan31@users.noreply.github.com>

--- projet.py
+++ projet.py
@@ -1,7 +1,34 @@
 # 1 -Import et definitions des variables globales
-"""fghjklm"""
+
+from collections import namedtuple
+import csv
+import decimal
+
+
+
+
+
 # 2 -Definition des fonctions secondaires
 
+def read_data_to_dicts(filename):
+    data = []
+    with open (filename, mode="r", encoding="utf8") as f:
+        for row in f:
+            data.extend(f)
+        f.closed
+        return data
+
 # 3 -Definition du main() qui appellent les fonctions secondaires
+def main():
+    #récupération du fichier
+    #with open('Annual_Surface_Temperature_Change.csv','r','utf8') as f :
+    #    data=f.read()            
+    #f.closed
+
+    print(read_data_to_dicts('Annual_Surface_Temperature_Change.csv'))
+    
+    pass
 
 # 4 -Appel protégé du main()
+if __name__ == '__main__':
+    main()
