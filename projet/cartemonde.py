074fa7d972643160a79420241de6f4dfc85c054e
selma.zine-eddine@edu.esiee.fr <selma.zine-eddine@edu.esiee.fr>
2023-12-27 19:10:25 +0100

    carte interactif

--- projet/cartemonde.py
+++ projet/cartemonde.py
@@ -9,7 +9,8 @@ from cartopy import crs
 from geoviews import dim
 import pandas as pd
 import folium
-
+import plotly.express as px
+import plotly
 
 ### CARTE ###
 def carte():
@@ -35,9 +36,78 @@ def carte():
     map.save(outfile=os.path.join(script_dir, 'projetmap.html'))
     return map
 
+def interaction() :
+    # Obtenez le chemin du répertoire du script en cours d'exécution
+    script_dir = os.path.dirname(__file__)
+    df = pd.read_csv(os.path.join(script_dir,'Annual_Surface_Temperature_Change.csv'))
+    pays = df['Country']
+    coef = df['F2022']
+
+    df['text']='Country : '+ df['Country'].astype(str)+'Increasment Coefficient '+ df['F2022'].astype(str)
+
+    data = [dict(type='choropleth',autocolorscale=False, locations = df['Country'], z=df['F2022'],
+             locationmode='world',text = df['text'], colorscale = 'Viridis', colorbar = dict(title='increasment coeff'))]
+    
+    layout = dict(title='Increasment coefficient',
+              geo = dict(scope='world',projection = dict(type ='world'),
+                        showlakes =True,lakecolor='rgb(66,165,245)'))
+
+    plotly.offline.iplot({
+    "data": data,
+    "layout": layout
+    })
+
+    return
+
+def pays(chaine):
+    p = chaine.split()
+    return p[0]
+
+
+
+def carte2() :
+    script_dir = os.path.dirname(__file__)
+    df = pd.read_csv(os.path.join(script_dir,'Annual_Surface_Temperature_Change.csv'))
+    
+    coords = (0,0)
+    map = folium.Map(location=coords, tiles='OpenStreetMap', zoom_start=2)
+    # Construisez le chemin relatif au fichier GeoJSON
+    geojson_path = os.path.join(script_dir, 'countries.geojson')
+
+    df['text']='Country : '+ df['Country'].astype(str)+'Increasment Coefficient '+ df['F2022'].astype(str)
+  
+    cp = folium.Choropleth(
+        geo_data=geojson_path,                              # geographical data
+        name='choropleth',
+        data=df,                                  # numerical data
+        columns=[pays('Country'), 'F2022'],                     # numerical data key/value pair
+        key_on='feature.properties.name',       # geographical property used to establish correspondance with numerical data
+        fill_color='YlGn',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name='Temperature change coefficient'
+    ).add_to(map)
+    
+    df_indexed = df.set_index('Country')
+
+    for s in cp.geojson.data['features']:
+
+        try:
+            
+            s['properties']['F2022']=df_indexed.loc[s['properties']['name'],'F2022']
+        except KeyError:
+
+            s['properties']['F2022'] = 'error'
+
+
+    folium.GeoJsonTooltip(['F2022','name']).add_to(cp.geojson)
+    folium.LayerControl().add_to(map)
+
+    
+    map.save(outfile=os.path.join(script_dir, 'map.html'))
 
 def main() :
-    carte()
+    carte2()
     pass
 
 if __name__ == '__main__':
--- projet/projet.py
+++ projet/projet.py
@@ -1,5 +1,6 @@
 # 1 -Import et definitions des variables globales
-
+import os
+import sys
 from collections import namedtuple
 import csv
 import decimal
@@ -23,10 +24,10 @@ import folium
 # 2 -Definition des fonctions secondaires
 
 def read_data_to_dicts(filename):
+    
+
     data = []
     with open (filename, mode="r", encoding="utf8") as f:
-        #for row in f:
-        #    data.extend(f)
         data = pd.read_csv(filename)
         f.closed
         return data
@@ -52,91 +53,18 @@ def historigramme(data):
 
 def colonne(data, nomColonne):
     return data[nomColonne]
-
-def multicolonne(data,nom1, nom2):
-    c= [name for name in data.columns if nom1 or nom2 in name ]
-    return data[c]
-
-
-def obtenir_geojson_pays():
-    # Utilisez la bibliothèque GeoPandas pour lire le GeoJSON des frontières des pays
-    world = pd.read_json('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
-    return world
-
-
-def obtenir_coordonnees_pays(nom_pays):
-    geolocator = Nominatim(user_agent="mon_application")
-    location = geolocator.geocode(nom_pays)
-    
-    if location:
-        return location.latitude, location.longitude
-    else:
-        print(f"Les coordonnées pour le pays '{nom_pays}' n'ont pas pu être trouvées.")
-        return None
-    
-def colorer_pays(nom_pays, couleur,map):
-    coords = obtenir_coordonnees_pays(nom_pays)
-    
-    if coords:
-        #map = folium.Map(location=coords, tiles='OpenStreetMap', zoom_start=5)
-        
-        # Ajouter un polygone pour représenter les frontières du pays
-        #folium.RegularPolygonMarker(location=coords, fill_color=couleur).add_to(map)
-        folium.Marker(location=coords, icon=folium.Icon(color=couleur)).add_to(carte)
-        # Sauvegarder la carte dans un fichier HTML
-        map.save(outfile=f'map.html')
-        
-        #print(f"Carte pour {nom_pays} générée avec succès.")
-    else:
-        print("Impossible de générer la carte.")
-
-def carte():
-    coords = (0,0)
-    map = folium.Map(location=coords, tiles='OpenStreetMap', zoom_start=2)
-    map.save(outfile='map.html')
-    return map
-
-def pays(chaine):
-    p = chaine.split()
-    return p[0]
-
-def paysPlusChaud(data, map):
-    NomPays= colonne(data,"Country")
-    coeffAugmentation = colonne(data,"F2022")
-    
-    for nom_pays, coef in zip(NomPays, coeffAugmentation):
-        if coef > 1.5:
-            colorer_pays(pays(nom_pays),'red',map)
-
     
 # 3 -Definition du main() qui appellent les fonctions secondaires
 def main():
-    data = read_data_to_dicts('Annual_Surface_Temperature_Change.csv')   
-    #carte_instance = carte()
-    #paysPlusChaud(data, carte_instance)
+    # Obtenez le chemin du répertoire du script en cours d'exécution
+    script_dir = os.path.dirname(__file__)
+
+    data = read_data_to_dicts(os.path.join(script_dir,'Annual_Surface_Temperature_Change.csv'))   
     historigramme(data)
     pass
-    #historigramme(data)
+
 # 4 -Appel protégé du main()
 if __name__ == '__main__':
     main()
-    #app = dash.Dash(__name__)
 
-    #df = pd.read_csv('Annual_Surface_Temperature_Change.csv')
-
-    #fig = px.scatter(df, x="Country", y="F1981",
-    #                size="F1981", color="Country", hover_name="Country",
-    #                log_x=True, size_max=60)
-
-    #app.layout = html.Div([
-    #    dcc.Graph(
-    #        id='life-exp-vs-gdp',
-    #        figure=fig
-    #    )
-    #])
-
-    #app.run_server(debug=True)
-
-    
-    #dataGeolocalisation = read_data_to_dicts('Annual_Surface_Temperature_Change.csv')
     
